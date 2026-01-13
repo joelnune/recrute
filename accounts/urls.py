3305dd5e30de0149acda71b3cfb715b124bd8884
joelnune <joelnunes974@gmail.com>
2026-01-13 20:19:04 -0300

    autenticação do usuário com redirecionamento baseado no tipo

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,11 +4,13 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('login/', views.login_view, name='login'),
+    path('login/', views.efetuar_login, name='efetuar_login'),
     path('logout/', views.logout_view, name='logout'),
 
-path('cadastro/', views.cadastro, name='cadastro'),
+    path('cadastro/', views.cadastro, name='cadastro'),
 
     path('cadastrar/candidato/', views.cadastrar_candidato, name='cadastrar_candidato'),
     path('cadastrar/recrutador/', views.cadastrar_recrutador, name='cadastrar_recrutador'),
+
+
 ]
